3b1e2de33f89dc5785069bbebefe95d35afcee17
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-06-04 11:53:00 +0200

    Ajout de commentaires dan Bataille

--- src/main/java/tpjava/Bataille.java
+++ src/main/java/tpjava/Bataille.java
@@ -13,22 +13,29 @@ public class Bataille {
 	public void start() {
 		Carte c1, c2;
 		int nbManche = 0;
+
 		while (true) {
 			nbManche++;
+			System.out.println("Manche " + nbManche);
+
+			// les joueurs tirent une carte
 			c1 = j1.draw();
 			c2 = j2.draw();
 
-			System.out.println("Manche " + nbManche);
 			if (c1 != null && c2 != null) {
 				System.out.println("J1 | " + c1.getValeur() + " of " + c1.getCouleur() + " - " + +c2.getValeur()
 						+ " of " + c2.getCouleur() + " | J2");
 
 				while (c1.equals(c2)) {
+
+					// egalite dans la manche, nouvelle carte tiree
 					pile.addCard(c1);
 					pile.addCard(c2);
 					c1 = j1.draw();
 					c2 = j2.draw();
+
 					if (c1 == null || c2 == null) {
+						// un des deux decks est vide
 						break;
 					}
 					System.out.println("J1 | " + c1.getValeur() + " of " + c1.getCouleur() + " - " + +c2.getValeur()
@@ -37,6 +44,8 @@ public class Bataille {
 			}
 
 			if (c1 == null || c2 == null) {
+				// un des deux decks est vide
+
 				if (c1 == null && c2 == null) {
 					// egalite
 					System.out.println("Egalite");
@@ -59,6 +68,7 @@ public class Bataille {
 				}
 
 			} else if (c1.beats(c2)) {
+				// j1 gagne la manche
 
 				j1.addCard(c1);
 				j1.addCard(c2);
@@ -66,17 +76,17 @@ public class Bataille {
 					j1.addCard(pile.draw());
 				}
 
-				System.out.println("J1 gagne le round");
+				System.out.println("J1 gagne la manche");
 				System.out.println("J1 | " + j1.getSize() + " - " + j2.getSize() + " | J2");
 
 			} else {
-
+				// j2 gagne la manche
 				j2.addCard(c1);
 				j2.addCard(c2);
 				while (pile.getSize() > 0) {
 					j2.addCard(pile.draw());
 				}
-				System.out.println("J2 gagne le round");
+				System.out.println("J2 gagne la manche");
 				System.out.println("J1 | " + j1.getSize() + " - " + j2.getSize() + " | J2");
 
 			}
